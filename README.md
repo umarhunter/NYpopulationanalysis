ebcbbc5f32e4856f8228cc6887ed9f4d488d0257
umarhunter <umor.uddin54@myhunter.cuny.edu>
2022-05-30 19:23:27 -0400

    version 2 WIP, almost done

--- README.md
+++ README.md
@@ -2,9 +2,21 @@
 
 Program Description: Uses Python's Pandas, Matplotlib, and other libraries to analyze population data for the state of New York. Allows user to select from a variety of options to analyze data.
 
-**Current Version:** 1
+**Current Version:** 2
 
 **Features:**
+- simplified code by adding more task focused functions
+- allows user to choose specific county and year
+- added some statistic to fast facts such as expected growth or decay
+- compare population growth between counties
+- variety of graphical options based off user input
+
+**Upcoming Version:** 3
+**Features:**
+TBD -- will most likely be final version and compromise of minor fixes and reorganization
+- extreme changes in population (curves in graphs) will be colored red and green.
+**Previous Versions:**
+**Version 1**
 - reads CSV file
 - instructions/tutorial
 - allows user inputs (only for county)
@@ -12,14 +24,3 @@ Program Description: Uses Python's Pandas, Matplotlib, and other libraries to an
 - statistical information based off county
 - contains necessary functionality for implementation in version 2
 - error checking
-
-**Upcoming Version:** 2
-**Features:**
-- allows user to choose specific county and year
-- compare population growth between counties
-- variety of graphical options based off user input
-
-
-**Upcoming Version:** 3
-**Features:**
-- n/a
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Property of Umar Faruque
 # See readme for documentation
 import csv
-
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import pandas as pd
 
 
@@ -17,15 +17,115 @@ def tutorial():
           "of available counties. Newly registered counties after 2020 are not supported.\n*****************************************************")
 
 
-def fastfacts():
-    maxYear = sortCounty.loc[sortCounty['Population'].idxmax(), 'Year']
-    minYear = sortCounty.loc[sortCounty['Population'].idxmin(), 'Year']
-    print("The highest population was", sortCounty['Population'].max(), "in", maxYear)
-    print("The lowest population was", sortCounty['Population'].min(), "in", minYear)
+def fastFacts():
+    maxYear = sortcounty.loc[sortcounty['Population'].idxmax(), 'Year']
+    minYear = sortcounty.loc[sortcounty['Population'].idxmin(), 'Year']
+    print("The highest population was", sortcounty['Population'].max(), "in",
+          maxYear)  # we can also use sortcounty['Population'].value_counts()[1]
+    print("The lowest population was", sortcounty['Population'].min(), "in", minYear)
 
 
 def printCountries():
-    print('\n'.join(map(str, population['Geography'].unique().tolist())))
+    truthvar = 0
+    while truthvar == 0:
+        listCounty = input("Would you like to see the list of counties available in this dataset? y/n: ")
+        if listCounty == 'y':
+            truthvar = 1
+            print('\n'.join(map(str, population['Geography'].unique().tolist())))
+            print("\nPrinting county list complete. Please scroll up to see the list.\n")
+        elif listCounty == 'n':
+            truthvar = 1
+            pass
+        else:
+            print("Invalid input.")
+
+
+def askTutorial():
+    truthvar = 1
+    while truthvar > 0:
+        tutorialinput = input("Tutorial? y/n: ")
+        if tutorialinput == 'y':
+            tutorial()
+            truthvar = 0
+        elif tutorialinput == 'n':
+            truthvar = 0
+            pass
+        else:
+            print("Invalid input. Enter y or n.")
+
+
+def getValidCounty():
+    truthvar = 1
+    while truthvar == 1:
+        county = input(
+            "Please enter a valid county: ").title()  # get user input and capitalize it in case user submits lowercase characters
+        with open("annualpopulation.csv") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Geography'] == county:
+                    truthvar = 0
+                    return county
+                    break
+
+
+def askCompareCounty(county):
+    truthvar = 1
+    while (truthvar == 1):
+        # useroption = input("Would you like to compare this county to others?\nPlease enter y/n: ")
+        useroption = 'y'
+        if useroption == 'y':
+            truthvar = 0
+            # county2 = getValidCounty()
+            county2 = 'Albany County'
+            beforeCompareCounty(county, county2)
+        elif useroption == 'n':
+            truthvar = 0
+        else:
+            print("Invalid entry. Please try again.")
+
+
+def beforeCompareCounty(county1, county2):
+    truthvar = 0
+    while truthvar == 0:
+        # askinterval = input(
+        #    "Would you like to compare these two counties within a specific time interval?\nPlease enter y/n: ")
+        askinterval = 'y'
+        if askinterval == 'y':
+            truthvar = 1
+            #start = int(input("Enter starting year: "))
+            start = 1998
+            while start < 1970 or start > 2020:
+                print("Invalid starting year. Must be between 1970 and 2020.")
+                start = input("Enter starting year: ")
+            #end = int(input("Enter ending year: "))
+            end = 2005
+            while end < 1970 or end > 2020:
+                print("Invalid ending year. Must be between 1970 and 2020.")
+                end = input("Enter ending year: ")
+            compareCounty(county1, county2, start, end)
+        elif askinterval == 'n':
+            truthvar = 1
+            break
+        else:
+            print("Invalid response. Please try again.")
+
+
+def compareCounty(county1, county2, startperiod, endperiod):
+    #outputfile = input("Please enter the name of the output file: ")
+    from matplotlib.ticker import ScalarFormatter
+    plt.gca().get_yaxis().get_major_formatter().set_useOffset(False)
+
+    outputfile = 'file.png'
+    sortcounty1 = population.groupby('Geography').get_group(county1)
+    plt.plot('Year','Population', 'b', label = county1, data=sortcounty1)
+    sortcounty2 = population.groupby('Geography').get_group(county2)
+    plt.plot('Year','Population', 'r', label = county2, data=sortcounty2)
+
+    plt.title('Population between ' + county1 + ' and ' + county2 + ' from ' + str(startperiod) + ' to ' + str(endperiod),
+              color='black')
+    plt.legend(loc='best')
+    graph = plt.gcf()
+    graph.savefig(outputfile)
 
 
 if __name__ == '__main__':
@@ -35,50 +135,22 @@ if __name__ == '__main__':
     print("************************************************************************************************\n         "
           "                   This program was created by: Umar Faruque                            "
           "\n************************************************************************************************\n")
-    truthVar = 1
-    while truthVar > 0:
-        tutorialInput = input("Tutorial? y/n: ")
-        if tutorialInput == 'y':
-            tutorial()
-            truthVar = 0
-        elif tutorialInput == 'n':
-            truthVar = 0
-            pass
-        else:
-            print("Invalid input. Enter y or n.")
-
-    while truthVar == 0:
-        listCounty = input("Would you like to see the list of counties available in this dataset? y/n: ")
-        if listCounty == 'y':
-            truthVar = 1
-            printCountries()
-            print("\nPrinting county list complete. Please scroll up to see the list.\n")
-        elif listCounty == 'n':
-            truthVar = 1
-            pass
-        else:
-            print("Invalid input.")
-
-    #while truthVar == 1:
-    #    county = input("Please enter a valid county: ").title() #get user input and capitalize it in case user submits lowercase characters
-    #    county = county.title()  # capitalizes input so that lower case works
-    #    with open("annualpopulation.csv") as csvfile:
-    #        reader = csv.DictReader(csvfile)
-    #        for row in reader:
-    #            if row['Geography'] == county:
-    #                truthVar = 0
-    #                break
-
-    county = 'Queens County'.title()  # used for speeding through test
+    # askTutorial()
+    # printCountries()
+    # county = getValidCounty()
+    county = 'Queens County'
     print("In order to see graphs, you must enter an output file, otherwise you will only see statistical information "
           "in the console.\nPlease note: if you would like to create a PDF then append your file with .pdf. The same "
           "logic applies for other file types.")
-    outputFile = input("Please enter the name of the output file: ")
-
-    sortCounty = population.groupby('Geography').get_group(county)
-    sortCounty.plot(x='Year', y='Population')
+    # outputfile = input("Please enter the name of the output file: ")
+    outputfile = 'file.png'
+    sortcounty = population.groupby('Geography').get_group(county)
+    sortcounty.plot(x='Year', y='Population')
+    plt.title('Population in ' + county + " from 1970 to 2020", color='black')
     graph = plt.gcf()
-    graph.savefig(outputFile)
-    print("Population Data for: ", county)
-    fastfacts()
-
+    graph.savefig(outputfile)
+    print("\n********************\nSUCCESS. YOUR FILE HAS BEEN SAVED IN THE LOCAL DIRECTORY.\nPopulation Data for: ",
+          county)
+    fastFacts()
+    askCompareCounty(county)
+    print("Thank you for using my program!")
